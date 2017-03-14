81f941fafbf85988467f44e3614049c2d6d3b15d
thaker-nish <thaker.nish@gmail.com>
2017-03-14 13:48:17 -0500

    Build out LF_challenge_consumer_scoring_spec.rb file, require webmock in spec_helper.rb file

--- lib/LF_challenge_consumer_scoring.rb
+++ lib/LF_challenge_consumer_scoring.rb
@@ -1,7 +1,7 @@
 require 'json'
 require 'rest_client'
-require 'LF_challenge_consumer_scoring/consumer'
-require 'LF_challenge_consumer_scoring/version'
+require "LF_challenge_consumer_scoring/consumer"
+require "LF_challenge_consumer_scoring/version"
 
 module LFChallengeConsumerScoring
   # Your code goes here...
--- spec/LF_challenge_consumer_scoring_spec.rb
+++ spec/LF_challenge_consumer_scoring_spec.rb
@@ -5,7 +5,42 @@ describe LFChallengeConsumerScoring do
     expect(LFChallengeConsumerScoring::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    describe '#initialize' do
+  it 'should have a vaild url' do
+    consumer = ConsumerScoring::Consumer.new
+    url = 'http://not_real.com/customer_scoring'
+    expect(consumer.send(:url)).to eq(url)
+  end
+       it "should allow another url to be specified" do
+      custom_url = "http://paulnsorensen.com"
+      client = ConsumerScoring::Consumer.new(custom_url)
+      expect(client.send(:url)).to eql(custom_url)
+    end
+  end
+  describe "#scoring" do
+    context "when successful" do
+      before(:all) do
+        test_url =  ConsumerScoring::Consumer::URL
+        test_url += "?age=35&income=50000&zipcode=60621"
+        stub_request(:get, "http://not_real.com/customer_scoring?age=35&income=50000&zip_code=60621").to_return(status: 200, body: '{propensity: 0.26532,ranking: "C"}')
+        consumer = ConsumerScoring::Consumer.new
+        @result = consumer.scoring("50000", "60621", "35")
+        @result = eval(@result)
+      end
+
+      it "should return an hash" do
+        expect(@result).to be_kind_of(Hash)
+      end
+      it "should only include 'propensity' and 'ranking" do
+        expect(@result.keys.sort).to eql([:propensity, :ranking])
+      end
+      it "should have the correct value for 'propensity'" do
+        expect(@result[:propensity]).to be(0.26532)
+      end
+
+      it "should have the correct value for 'ranking'" do
+        expect(@result[:ranking]).to eql('C')
+      end
+    end
   end
 end
