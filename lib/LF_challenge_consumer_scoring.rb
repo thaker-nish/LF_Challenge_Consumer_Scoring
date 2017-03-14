7a529f03a40fcdeb4620bee1a49b8d90eae17d2a
thaker-nish <thaker.nish@gmail.com>
2017-03-14 13:26:21 -0500

    Require json, rest_client, and consumer.rb file in LF_challenge_consumer_scoring.rb file

--- lib/LF_challenge_consumer_scoring.rb
+++ lib/LF_challenge_consumer_scoring.rb
@@ -1,4 +1,7 @@
-require "LF_challenge_consumer_scoring/version"
+require 'json'
+require 'rest_client'
+require 'LF_challenge_consumer_scoring/consumer'
+require 'LF_challenge_consumer_scoring/version'
 
 module LFChallengeConsumerScoring
   # Your code goes here...
